254b0d7a70ddf3d749fe00607da51000ddb33444
druphliu <hui.liu@yingxiong.com>
2018-12-10 00:14:00 +0800

    import backend theme

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -22,7 +22,7 @@ class SiteController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['login', 'error'],
+                        'actions' => ['login', 'error','captcha'],
                         'allow' => true,
                     ],
                     [
@@ -49,6 +49,19 @@ class SiteController extends Controller
         return [
             'error' => [
                 'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+                'backColor'=>0x000000,//背景颜色
+                'maxLength' => 6, //最大显示个数
+                'minLength' => 5,//最少显示个数
+                'padding' => 5,//间距
+                'height'=>40,//高度
+                'width' => 130,  //宽度
+                'foreColor'=>0xffffff,     //字体颜色
+                'offset'=>4,        //设置字符偏移量 有效果
+                //'controller'=>'login',        //拥有这个动作的controller
             ],
         ];
     }
